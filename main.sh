49454886cc9c1c78208f51198832b18146ee0b60
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-19 00:01:25 -0200

    add method request and init method parser

--- main.sh
+++ main.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
-body=$(curl -s https://www.apple.com/)
+#body=$(curl -s https://www.apple.com/)
 
-parser="$body"
+#parser="$body"
+
+#!/bin/sh
+# A simple script with a function...
+
+_request()
+{
+  URL=$1
+  body=$(curl -s $URL)
+  parser="$body"
+  
+}
+
+_parser_text()
+{
+
+}
+
+###
+# Main
+###
+echo "Start of script..."
+_request https://www.apple.com/
+echo "End of script..."
