11ed957d61fcf8bde382bb44ef99cb393761f3ad
EduardoSantosGit <eduardosantos058@gmail.com>
2018-01-19 00:39:38 -0200

    add init method indexof

--- main.sh
+++ main.sh
@@ -1,28 +1,22 @@
-#!/bin/bash
-
-#body=$(curl -s https://www.apple.com/)
-
-#parser="$body"
-
 #!/bin/sh
-# A simple script with a function...
 
-_request()
+_request_get()
 {
   URL=$1
   body=$(curl -s $URL)
   parser="$body"
-  
 }
 
-_parser_text()
+_index_of()
 {
-
+    STRING=$1
+    echo $STRING | grep -aob '' | grep -oE '[0-9]+'
 }
 
 ###
 # Main
 ###
 echo "Start of script..."
-_request https://www.apple.com/
+_request_get https://www.apple.com/
+_index_of $parser
 echo "End of script..."
